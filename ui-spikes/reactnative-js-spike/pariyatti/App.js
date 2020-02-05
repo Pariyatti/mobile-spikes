c9f7a7c471787299acda6f67a661394db4ac41d3
Maciej Falski <takaczapka@gmail.com>
2020-02-05 11:24:33 +0000

    Add sample text input

--- ui-spikes/reactnative-js-spike/pariyatti/App.js
+++ ui-spikes/reactnative-js-spike/pariyatti/App.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet, Text, TextInput, View } from 'react-native';
 import * as Localization from 'expo-localization';
 import i18n from 'i18n-js';
 
@@ -15,13 +15,31 @@ i18n.locale = Localization.locale;
 // When a value is missing from a language it'll fallback to another language with the key present.
 i18n.fallbacks = true;
 
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { text: '' };
+  }
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>{i18n.t('welcome')} Pariyatti!</Text>
-    </View>
-  );
+  render() {
+    return (
+      <View style={styles.container}>
+
+        <Text>(I18N) {i18n.t('welcome')} Pariyattssi!</Text>
+
+        <TextInput
+          style={{ height: 40, width: 200, color: "#FF9F33", backgroundColor: "#E1E1E1" }}
+
+          placeholder="Input here"
+          onChangeText={(text) => this.setState({ text })}
+          value={this.state.text}
+        />
+        <Text style={{ padding: 10, fontSize: 12 }}>
+          I've just typed this: {this.state.text}
+        </Text>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
