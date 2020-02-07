9bb92285b45e26fad7520c08be11606be11b9826
Maciej Falski <takaczapka@gmail.com>
2020-02-07 11:50:31 +0000

    Display today's date on Today screen

--- ui-spikes/reactnative-js-spike/pariyatti/today-stack.js
+++ ui-spikes/reactnative-js-spike/pariyatti/today-stack.js
@@ -3,6 +3,7 @@ import { Button, Text, View } from 'react-native';
 import { createStackNavigator } from 'react-navigation-stack';
 import * as Localization from 'expo-localization';
 import i18n from 'i18n-js';
+import Moment from 'moment';
 
 // Set the key-value pairs for the different languages you want to support.
 i18n.translations = {
@@ -12,11 +13,14 @@ i18n.translations = {
   hi: { welcome: "नमस्ते" }
 }
 
-class TodayScreen extends React.Component {
+class TodayScreen extends React.Component {  
   render() {
+    Moment.locale(i18n.locale);
+
     return (
       <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text>Today!</Text>
+        <Text> {Moment(new Date()).format('dddd, Do MMMM')} </Text>
+        <Text>Today</Text>
         <Text>(I18N) {i18n.t('welcome')} Pariyatti!</Text>
         <Button
           title="Go to Resources"
