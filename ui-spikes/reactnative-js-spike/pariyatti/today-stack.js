77e69f4121d2449ab845d0c62f425838bf7883a4
Maciej Falski <takaczapka@gmail.com>
2020-02-07 13:10:41 +0000

    Add embedded youtube video in Today's screen

--- ui-spikes/reactnative-js-spike/pariyatti/today-stack.js
+++ ui-spikes/reactnative-js-spike/pariyatti/today-stack.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { StyleSheet, Button, Text, Image } from 'react-native';
+import { WebView } from 'react-native-webview';
 import { ScrollView } from 'react-navigation'
 import { createStackNavigator } from 'react-navigation-stack';
 import Constants from 'expo-constants';
@@ -16,29 +17,38 @@ i18n.translations = {
   hi: { welcome: "नमस्ते" }
 }
 
+i18n.locale = Localization.locale;
+i18n.fallbacks = true;
+
+Moment.locale(Localization.locale);
+
 class TodayScreen extends React.Component {
   render() {
-    Moment.locale(i18n.locale);
-
     return (
       <SafeAreaView style={styles.container}>
         <ScrollView style={styles.scrollView}>
           <Text> {Moment(new Date()).format('dddd, Do MMMM')} </Text>
           <Text>Today</Text>
-          <Text>(I18N) {i18n.t('welcome')} Pariyatti!</Text>
+          <Text>{i18n.t('welcome')} Pariyatti! (i18n locale: {Localization.locale})</Text>
           <Button
             title="Go to Resources"
             onPress={() => this.props.navigation.navigate('Resources')}
+          />
+          <Image
+            style={{ width: 150, height: 150 }}
+            source={require('./resources/goenka.jpeg')}
           />
           <Text style={styles.text}>
             With good will for the entire cosmos,
             cultivate a limitless heart:
             Above, below, and all around,
             unobstructed, without hostility or hate.
-        </Text>
-          <Image
-            style={{ width: 150, height: 150 }}
-            source={require('./resources/goenka.jpeg')}
+          </Text>
+          <WebView
+            style={{ flex: 1 }}
+            javaScriptEnabled={true}
+            source={{ uri: 'https://www.youtube.com/embed/heobdcl_iO4' }}
+            style={{ width: 300, height: 300 }}
           />
           <Text style={styles.text}>
             This is the law of nature,
@@ -46,6 +56,7 @@ class TodayScreen extends React.Component {
             a defiled mind remains agitated,
             an unstained mind is happy.
         </Text>
+
         </ScrollView>
       </SafeAreaView>
     );
