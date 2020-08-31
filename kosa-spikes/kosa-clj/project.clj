e66094c06fba83365ac89a532e91b61eea2acf89
udit <yudistrange@gmail.com>
2020-08-31 09:02:04 +0530

    Adds neo4j-clj deps to interop with neo4j database
    Also adds some test functions to play around with the database

--- kosa-spikes/kosa-clj/project.clj
+++ kosa-spikes/kosa-clj/project.clj
@@ -3,5 +3,7 @@
   :url "http://pariyatti.org"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.1"]]
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [gorillalabs/neo4j-clj "4.0.1"]
+                 [org.neo4j.test/neo4j-harness "4.0.0"]]
   :repl-options {:init-ns kosa-clj.core})
--- kosa-spikes/kosa-clj/src/kosa_clj/core.clj
+++ kosa-spikes/kosa-clj/src/kosa_clj/core.clj
@@ -1,6 +1,40 @@
-(ns kosa-clj.core)
+(ns kosa-clj.core
+  (:require[neo4j-clj.core :as neo4j])
+  (:import (java.net URI)))
 
 (defn foo
   "I don't do a whole lot."
   [x]
   (println x "Hello, World!"))
+
+(neo4j/defquery create-book
+  "CREATE (u:book $book)")
+
+(neo4j/defquery get-all-books
+  "Match (u:book) RETURN u as book")
+
+(def db
+  (neo4j/connect (URI. "bolt://localhost:7687")
+                 "neo4j"
+                 "neo4j"))
+
+(defn create-book-fn [book]
+  (with-open [session (neo4j/get-session db)]
+    (create-book session book)))
+
+(create-book-fn {:book {:name "alice in wonderland" :author "lewis carroll"}})
+
+(defn get-book-fn []
+  (neo4j/with-transaction db txn
+    (prn (get-all-books txn))))
+
+;; Writing works with session and txn both
+(neo4j/with-transaction db txn
+  (create-book txn {:book {:name "alice in wonderland" :author "lewis carroll" :txn true}}))
+
+;; Querying doesn't work with session?
+(with-open [session (neo4j/get-session db)]
+  (get-all-books session))
+
+(get-book-fn)
+
