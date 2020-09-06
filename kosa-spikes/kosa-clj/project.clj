7248dd9d5e2b0f2c1120b00787457a9ad9b3d660
udit <yudistrange@gmail.com>
2020-09-06 10:53:30 +0530

    Adds deps for logging via timbre

--- kosa-spikes/kosa-clj/project.clj
+++ kosa-spikes/kosa-clj/project.clj
@@ -5,6 +5,8 @@
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [gorillalabs/neo4j-clj "4.0.1"]
-                 [org.neo4j.test/neo4j-harness "4.0.0"]]
+                 [org.neo4j.test/neo4j-harness "4.0.0"]
+                 [com.taoensso/timbre "4.10.0"]
+                 [com.fzakaria/slf4j-timbre "0.3.19"]]
   :main kosa-clj.core
   :repl-options {:init-ns kosa-clj.core})
